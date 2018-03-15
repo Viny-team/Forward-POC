feeb280325602ac8cb3bdde7dcf4a0381f62c219
scorsi <sylvain.corsini@gmail.com>
2018-03-15 22:47:12 +0100

    Fresh restart

--- old/components/FetchLocation.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Button} from 'react-native'
-
-export default fetchLocation = props => {
-    return (
-        <Button title='Get Location' onPress={props.onGetLocation} />
-    )
-}

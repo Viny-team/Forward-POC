abeb44ce6dfcae49a867485fa0547ed48038829e
scorsi <sylvain.corsini@gmail.com>
2018-03-23 00:33:40 +0100

    Add tabnavigator to browse all articles

--- src/screens/articles.js
+++ src/screens/articles.js
@@ -12,6 +12,7 @@ import {
 } from "react-native-ui-kitten"
 import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 import { DrawerWrapper, Separator } from "../components"
+import { TabNavigator, TabBarBottom } from "react-navigation"
 import _ from "lodash"
 const users = require("../assets/users.json").users
 
@@ -23,12 +24,7 @@ type States = {
   articles: any[]
 }
 
-export default class ArticlesScreen extends RkComponent<Props, States> {
-  static navigationOptions = {
-    drawerLabel: "Articles",
-    drawerIcon: <Icon name="newspaper" size={24} />
-  }
-
+class PersonalArticlesScreen extends RkComponent<Props, States> {
   constructor(props: Props) {
     super(props)
 
@@ -66,28 +62,144 @@ export default class ArticlesScreen extends RkComponent<Props, States> {
 
   render() {
     return (
-      <DrawerWrapper navigation={this.props.navigation}>
-        <View style={{ flex: 1, alignItems: "center" }}>
-          {(() => {
-            if (this.state.articles.length === 0) {
-              return <RkText>Oops, vous n'avez aucun articles...</RkText>
-            } else {
-              return (
-                <FlatList
-                  ref="bottles"
-                  style={{
-                    padding: 15,
-                    width: "100%"
-                  }}
-                  extraData={this.state}
-                  data={this.state.articles}
-                  renderItem={this._renderArticle}
-                  keyExtractor={item => item.title}
-                />
-              )
-            }
-          })()}
+      <View style={{ flex: 1, alignItems: "center" }}>
+        {(() => {
+          if (this.state.articles.length === 0) {
+            return <RkText>Oops, vous n'avez aucun articles...</RkText>
+          } else {
+            return (
+              <FlatList
+                ref="bottles"
+                style={{
+                  padding: 15,
+                  width: "100%"
+                }}
+                extraData={this.state}
+                data={this.state.articles}
+                renderItem={this._renderArticle}
+                keyExtractor={item => item.title}
+              />
+            )
+          }
+        })()}
+      </View>
+    )
+  }
+}
+
+class AllArticlesScreen extends RkComponent<Props, States> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = { username: "", articles: [] }
+    this._bootstrap()
+  }
+
+  async _bootstrap() {
+    const username: string = await AsyncStorage.getItem("username")
+    this.setState({
+      username,
+      articles: _.flatten(_.map(users, user => user.homepage))
+    })
+  }
+
+  _renderArticle(info: any) {
+    return (
+      <RkCard style={{ marginBottom: 25 }}>
+        <View rkCardHeader>
+          <RkText style={{ fontSize: 20 }}>{info.item.title}</RkText>
         </View>
+        <Image
+          rkCardImg
+          source={{
+            uri: info.item.image
+          }}
+        />
+        <View rkCardContent>
+          <RkText>{info.item.description}</RkText>
+        </View>
+      </RkCard>
+    )
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center" }}>
+        {(() => {
+          if (this.state.articles.length === 0) {
+            return <RkText>Oops, vous n'avez aucun articles...</RkText>
+          } else {
+            return (
+              <FlatList
+                ref="bottles"
+                style={{
+                  padding: 15,
+                  width: "100%"
+                }}
+                extraData={this.state}
+                data={this.state.articles}
+                renderItem={this._renderArticle}
+                keyExtractor={(item, i) => i.toString()}
+              />
+            )
+          }
+        })()}
+      </View>
+    )
+  }
+}
+
+export default class ArticlesScreen extends RkComponent<Props> {
+  static navigationOptions = {
+    drawerLabel: "Articles",
+    drawerIcon: <Icon name="newspaper" size={24} />
+  }
+
+  render() {
+    const Route = TabNavigator(
+      {
+        First: {
+          screen: PersonalArticlesScreen,
+          navigationOptions: {
+            title: "Personnalisés",
+            tabBarIcon: ({ tintColor }) => (
+              <Icon
+                name="account-check"
+                size={40}
+                style={{ color: "#661D32" }}
+              />
+            )
+          }
+        },
+        Second: {
+          screen: AllArticlesScreen,
+          navigationOptions: {
+            title: "Tous",
+            tabBarIcon: ({ tintColor }) => (
+              <Icon
+                name="all-inclusive"
+                size={40}
+                style={{ color: "#661D32" }}
+              />
+            )
+          }
+        }
+      },
+      {
+        tabBarComponent: props => {
+          return <TabBarBottom {...props} style={{ backgroundColor: "#fff" }} />
+        },
+        tabBarPosition: "bottom",
+        tabBarOptions: {
+          activeTintColor: "#661D32",
+          inactiveTintColor: "grey"
+        },
+        initialRouteName: "First"
+      }
+    )
+    return (
+      <DrawerWrapper navigation={this.props.navigation}>
+        <Route />
       </DrawerWrapper>
     )
   }
